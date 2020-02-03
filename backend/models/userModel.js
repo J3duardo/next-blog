86e33d5dc125896e337ce6c2917eea397396f9d0
J3duardo <jesus_69eduardo@hotmail.com>
2020-02-03 10:51:57 -0400

    Backend: Schema para creación de usuarios

---
+++ backend/models/userModel.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 30,
+    unique: true,
+    index: true,
+    lowercase: true
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 30
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  profile: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+  passwordConfirm: {
+    type: String,
+    required: true,
+    select: false
+  },
+  about: {
+    type: String
+  },
+  role: {
+    type: Number,
+    trim: true
+  },
+  photo: {
+    data: Buffer,
+    contentType: String
+  },
+  resetPasswordLink: {
+    data: String,
+    default: ""
+  }
+}, {timestamps: true});
+
+//Encriptar la contraseña del usuario
+userSchema.pre("save", async function(next) {
+  // En caso de ejecutar otra operación de guardado, no volver a encriptar la contraseña si ésta ya fue encriptada
+  if (!this.isModified("password")) {
+    return next()
+  }
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = null;
+  next();
+});
+
+//Verificar si la contraseña ingresada es correcta
+userSchema.methods.checkPassword = async function(providedPassword, realPassword) {
+  return await bcrypt.compare(providedPassword, realPassword);
+}
+
+module.exports = mongoose.model("User", userSchema);
