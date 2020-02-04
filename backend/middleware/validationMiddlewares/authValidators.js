5e81d6376780d0d3b0ae8a2c3a18c87a520b3586
J3duardo <jesus_69eduardo@hotmail.com>
2020-02-04 09:26:20 -0400

    Backend: Corrección de validación de data de signin y login

--- backend/middleware/validationMiddlewares/authValidators.js
+++ backend/middleware/validationMiddlewares/authValidators.js
@@ -2,20 +2,18 @@ const {check} = require("express-validator");
 
 // Validar data al registrar un nuevo usuario
 exports.signupValidator = [
-  check("username", "El username es obligatorio").exists(),
-  check("username", "El username debe tener entre 4 y 30 caracteres").isLength({min: 4, max: 30}),
-  check("name", "El nombre es obligatorio").exists(),
+  check("name", "El nombre es obligatorio").not().isEmpty(),
   check("name", "El nombre debe tener entre 5 y 30 caracteres").isLength({min: 5, max: 30}),
-  check("email", "El email es requerido").exists(),
+  check("email", "El email es requerido").not().isEmpty(),
   check("email", "Email inválido").isEmail(),
-  check("password", "La contraseña es requerida").exists(),
+  check("password", "La contraseña es requerida").not().isEmpty(),
   check("password", "La contraseña debe contener al menos 6 caracteres").isLength({min: 6, max: undefined}),
-  check("passwordConfirm", "Debe confirmar su contraseña").exists()
+  check("passwordConfirm", "Debe confirmar su contraseña").not().isEmpty()
 ];
 
 // Validar data al hacer login
 exports.loginValidator = [
-  check("email", "Debe ingresar su email").exists(),
+  check("email", "Debe ingresar su email").not().isEmpty(),
   check("email", "Email inválido.").isEmail(),
-  check("password", "Debe ingresar su contraseña")
+  check("password", "Debe ingresar su contraseña").not().isEmpty()
 ]
