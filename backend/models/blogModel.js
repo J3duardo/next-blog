0ff16e0a84ee36a6062e388e17159fbeef302c73
J3duardo <jesus_69eduardo@hotmail.com>
2020-02-07 08:03:36 -0400

    Backend: Schema para crear blogs

---
+++ backend/models/blogModel.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      min: 3,
+      max: 160,
+      index: true,
+      required: true
+    },
+    slug: {
+      type: String,
+      unique: true,
+      index: true
+    },
+    body: {
+      type: {},
+      required: true,
+      min: 200,
+      max: 2000000
+    },
+    excerpt: {
+      type: String,
+      required: true,
+      max: 1000
+    },
+    mtitle: {
+      type: String
+    },
+    mdescription: {
+      type: String
+    },
+    photo: {
+      data: Buffer,
+      contentType: String
+    },
+    category: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Category",
+        required: true
+      }
+    ],
+    tags: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Tag",
+        required: true
+      }
+    ],
+    postedBy: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User"
+    }
+  }, 
+  {timestamps: true}
+);
+
+module.exports = mongoose.model("Blog", blogSchema);
