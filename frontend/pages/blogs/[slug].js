5f7ae096a44436909cd9a23fa47917d85941d5b8
J3duardo <jesus_69eduardo@hotmail.com>
2020-02-11 10:10:09 -0400

    Frontend: Mostrar mensajes de error al cargar el blog

--- frontend/pages/blogs/[slug].js
+++ frontend/pages/blogs/[slug].js
@@ -45,18 +45,18 @@ const SingleBlog = (props) => {
   const head = () => {
     return (
       <Head>
-        <title>{blog.title} | {APP_NAME}</title>
+        <title>{!error ? blog.title : error} | {APP_NAME}</title>
         <meta
           name="description"
-          content={blog.mdescription}
+          content={!error ? blog.mdescription : error}
         />
         <meta
           property="og:title"
-          content={`${blog.title} | ${APP_NAME}`}
+          content={`${!error ? blog.title : error} | ${APP_NAME}`}
         />
         <meta
           property="og:description"
-          content={`${blog.mdescription}`}
+          content={`${!error ? blog.mdescription : error}`}
         />
         <meta
           property="og:type"
@@ -72,11 +72,11 @@ const SingleBlog = (props) => {
         />
         <meta
           property="og:image"
-          content={`${API}/api/blog/${blog.slug}/photo`}
+          content={!error ? `${API}/api/blog/${blog.slug}/photo` : ""}
         />
         <meta
           property="og:image:secure_url"
-          content={`${API}/api/blog/${blog.slug}/photo`}
+          content={!error ? `${API}/api/blog/${blog.slug}/photo` : ""}
         />
         <meta
           property="og:image:type"
@@ -87,53 +87,60 @@ const SingleBlog = (props) => {
     )
   }
 
+  const showError = () => {
+    return <div className="alert alert-danger text-center py-4"><h5>{error}</h5></div>
+  }
+
   return (
     <React.Fragment>
       {head()}
       <Layout>
         <main>
-          <article>
-            <div className="container-fluid">
-              <section className="mb-4">
-                <div className="row">
-                  <img
-                    ref={blogImgRef}
-                    className="img img-fluid blog-image"
-                    src={`${API}/api/blog/${blog.slug}/photo`}
-                    alt={`${blog.title}`}
-                    onError={loadDefaultImg}
-                  />
-                </div>
-              </section>
-              <section className="mb-4">
-                <div className="container">
-                  <h1 className="display-4 mb-4 text-center font-weight-bold">{blog.title}</h1>
-                  <p className="lead mark">
-                    Autor: {blog.postedBy.name} | Actualizado: {moment(blog.updatedAt).calendar()}
-                  </p>
-                </div>
-              </section>
-              <section className="mb-4">
-                <div className="container">
-                  {renderCategories()}
-                  {renderTags()}
-                </div>
-              </section>
-              <div className="container mb-4">
-                <section>
-                  <div className="col-md-12 px-0 lead">
-                    {reactHTML(blog.body)}
+          {error && showError()}
+          {!error &&
+            <article>
+              <div className="container-fluid">
+                <section className="mb-4">
+                  <div className="row">
+                    <img
+                      ref={blogImgRef}
+                      className="img img-fluid blog-image"
+                      src={`${API}/api/blog/${blog.slug}/photo`}
+                      alt={`${blog.title}`}
+                      onError={loadDefaultImg}
+                    />
                   </div>
                 </section>
+                <section className="mb-4">
+                  <div className="container">
+                    <h1 className="display-4 mb-4 text-center font-weight-bold">{blog.title}</h1>
+                    <p className="lead mark">
+                      Autor: {blog.postedBy.name} | Actualizado: {moment(blog.updatedAt).calendar()}
+                    </p>
+                  </div>
+                </section>
+                <section className="mb-4">
+                  <div className="container">
+                    {renderCategories()}
+                    {renderTags()}
+                  </div>
+                </section>
+                <div className="container mb-4">
+                  <section>
+                    <div className="col-md-12 px-0 lead">
+                      {reactHTML(blog.body)}
+                    </div>
+                  </section>
+                </div>
               </div>
-            </div>
-            <div className="container mb-4">
-              <h5 className="text-center">Blogs relacionados:</h5>
-            </div>
-            <div className="container mb-4">
-              <h5 className="text-center">Comentarios:</h5>
-            </div>
-          </article>
+              <div className="container mb-4">
+                <h5 className="text-center">Blogs relacionados:</h5>
+              </div>
+              <div className="container mb-4">
+                <h5 className="text-center">Comentarios:</h5>
+              </div>
+            </article>          
+          }
         </main>
       </Layout>
     </React.Fragment>
@@ -150,6 +157,11 @@ SingleBlog.getInitialProps = async (props) => {
     if(error.response) {
       return {
         error: error.response.data.message
+      }
+    }
+    if(error.message.includes("Network") || error.message.includes("ECONNREFUSED")) {
+      return {
+        error: "Error al cargar los datos."
       }
     }
     return {
