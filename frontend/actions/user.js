fcf4a1d04955d759aceb2b38bd034d4418fb9c1a
J3duardo <jesus_69eduardo@hotmail.com>
2020-02-15 12:33:30 -0400

    Frontend: Funcionalidad para buscar y actualizar perfil del usuario actual

--- frontend/actions/user.js
+++ frontend/actions/user.js
@@ -1,11 +1,37 @@
 import axios from "axios";
 import {API} from "../config";
 
+// Buscar perfil público del usuario
 export const getPublicUserProfile = async (username) => {
   return await axios({
     method: "GET",
     url: `${API}/api/user/${username}`,
     headers: {
+      Accept: "application/json"
+    }
+  })
+}
+
+// Buscar perfil del usuario actual
+export const getCurrentUserProfile = async (token) => {
+  return await axios({
+    method: "GET",
+    url: `${API}/api/user/profile`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json"
+    }
+  })
+}
+
+// Actualizar perfil del usuario actual
+export const updateCurrentUserProfile = async (userdata, token) => {
+  return await axios({
+    method: "PATCH",
+    url: `${API}/api/user/update`,
+    data: userData,
+    headers: {
+      Authorization: `Bearer ${token}`,
       Accept: "application/json"
     }
   })
