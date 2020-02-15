adf71f15235638b44fa97edfc6836eed084f3ec1
J3duardo <jesus_69eduardo@hotmail.com>
2020-02-15 10:16:01 -0400

    Backend: Controller para actualizar perfil del usuario

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,8 @@
 const User = require("../models/userModel");
 const Blog = require("../models/blogModel");
+const formidable = require("formidable");
+const fs = require("fs");
+const {validationResult} = require("express-validator");
 
 // Buscar el perfil público del usuario
 exports.publicProfile = async (req, res) => {
@@ -35,4 +38,110 @@ exports.publicProfile = async (req, res) => {
       error: error.message
     })
   }
+}
+
+// Actualizar perfil del usuario
+exports.updateUserProfile = async (req, res) => {
+  // Validar name y email
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({
+      status: "failed",
+      message: errors.array()[0].msg,
+      error: errors.array()[0].msg
+    })
+  }
+
+  try {
+    // Procesar la data ingresada en el formulario
+    const form = new formidable.IncomingForm();
+    form.parse(req, async (err, fields, files) => {
+      if(err) {
+        return res.status(400).json({
+          status: "failed",
+          message: "No se pudo procesar la información",
+          error: "No se pudo procesar la información"
+        })
+      }  
+
+      // Chequear si el usuario existe
+      const user = await User.findOne({email: req.user.email});      
+      if(!user) {
+        return res.status(404).json({
+          status: "failed",
+          message: "Usuario no encontrado",
+          error: "Usuario no encontrado"
+        })
+      }
+
+      // Extraer los campos del formulario
+      const {username, name, email, about} = fields;
+
+      // Validar el username
+      if(username === "") {
+        return res.status(400).json({
+          status: "failed",
+          message: "El username es obligatorio"
+        })
+      }
+      if(username.length > 30) {
+        return res.status(400).json({
+          status: "failed",
+          message: "El username no puede contener más de 30 caracteres"
+        })
+      }
+
+      // Validar el nuevo email ingresado
+      if(user.email !== email) {
+        const checkNewEmail = await User.findOne({email: email});
+        if(checkNewEmail) {
+          return res.status(400).json({
+            status: "failed",
+            message: "Ya existe un usuario asociado al email ingresado",
+            error: "Ya existe un usuario asociado al email ingresado"
+          })
+        }
+      }
+
+      // Validar que la foto del perfil no sea mayor de 1MB
+      if(files.photo) {
+        if(files.photo.size > 1000000) {
+          return res.status(400).json({
+            status: "failed",
+            message: "La foto no debe ser mayor de 1MB",
+            error: "La foto no debe ser mayor de 1MB"
+          });
+        }
+        user.photo.data = fs.readFileSync(files.photo.path);
+        user.photo.contentType = files.photo.type;
+      }
+
+      // Actualizar perfil y guardarlo en la base de datos
+      user.username = username;
+      user.name = name;
+      user.email = email;
+      user.about = about;
+      user.profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+      await user.save();
+
+      return res.json({
+        status: "success",
+        message: "Perfil actualizado exitosamente",
+        data: user
+      })
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      status: "failed",
+      message: "Internal server error",
+      error: error.message
+    })
+  }
+}
+
+// Buscar foto del perfil del usuario
+exports.getUserPhoto = async (req, res) => {
+
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const {protectRoute, authMiddleware, readProfile} = require("../controllers/authController");
-const {publicProfile} = require("../controllers/user");
+const {publicProfile, updateUserProfile, getUserPhoto} = require("../controllers/user");
+const {check} = require("express-validator");
 
+// Buscar el perfil del usuario
 router.get("/profile", protectRoute, authMiddleware, readProfile);
+// Buscar el perfil público del usuario
 router.get("/user/:username", publicProfile);
+// Actualizar el perfil del usuario
+router.patch("/user/update",
+  protectRoute,
+  authMiddleware, [
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("name", "El nombre debe tener entre 5 y 30 caracteres").isLength({min: 5, max: 30}),
+    check("email", "El email es requerido").not().isEmpty(),
+    check("email", "Email inválido").isEmail()
+  ],
+  updateUserProfile);
+// Buscar la foto del perfil del usuario
+router.get("/user/photo/:username", getUserPhoto);
 
 module.exports = router;
