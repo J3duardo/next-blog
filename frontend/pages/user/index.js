a70a1b9e60f1e7bb436104db688bec2f3bb1df7d
J3duardo <jesus_69eduardo@hotmail.com>
2020-02-15 12:35:31 -0400

    Frontend: Página para actualizar perfil del usuario actual

--- frontend/pages/user/index.js
+++ frontend/pages/user/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Layout from "../../components/Layout";
 import Private from "../../components/auth/Private";
 
@@ -5,7 +6,30 @@ const userIndex = () => {
   return (
     <Layout>
       <Private>
-        <h2>User dashboard</h2>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12 mb-5">
+              <h2>User dashboard</h2>
+            </div>
+            <div className="col-md-4">
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <a href="/user/create/blog">Crear Blog</a>
+                </li>
+                <li className="list-group-item">
+                  <Link href="/user/create/blogs">
+                    <a>Editar y Borrar Blogs</a>
+                  </Link>
+                </li>
+                <li className="list-group-item">
+                  <Link href="/user/update">
+                    <a>Editar perfil</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </Private>
     </Layout>
   )
