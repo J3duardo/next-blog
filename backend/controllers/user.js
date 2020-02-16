b1ccdf1a7418f9b2cb94a1bb7c76d4b2ca98331b
J3duardo <jesus_69eduardo@hotmail.com>
2020-02-15 21:37:39 -0400

    Backend: Refact. updateUserProfile para manejo de errores de data duplicada

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -43,136 +43,156 @@ exports.publicProfile = async (req, res) => {
 // Actualizar perfil del usuario actual
 exports.updateUserProfile = async (req, res) => {
   try {
+    // Chequear si el usuario existe
+    const user = await User.findOne({_id: req.user.userId}).select("+password");
+    if(!user) {
+      return res.status(404).json({
+        status: "failed",
+        message: "Usuario no encontrado",
+        error: "Usuario no encontrado"
+      })
+    }
+
     // Procesar la data ingresada en el formulario
     const form = new formidable.IncomingForm();
     form.parse(req, async (err, fields, files) => {
-      if(err) {
-        return res.status(400).json({
-          status: "failed",
-          message: "No se pudo procesar la información",
-          error: "No se pudo procesar la información"
-        })
-      }  
-
-      // Chequear si el usuario existe
-      const user = await User.findOne({_id: req.user.userId}).select("+password");
-      if(!user) {
-        return res.status(404).json({
-          status: "failed",
-          message: "Usuario no encontrado",
-          error: "Usuario no encontrado"
-        })
-      }
-
-      // Extraer los campos del formulario
-      const {username, name, email, currentPassword, password, passwordConfirm, about} = fields;
-
-      // Validar el username
-      if(username === "" || username.length < 5 || username.length > 30) {
-        return res.status(400).json({
-          status: "failed",
-          message: "El username debe contener entre 5 y 30 caracteres"
-        })
-      }
-
-      // Validar el nombre
-      if(name.length === "" || name.length < 5 || name.length > 30) {
-        return res.status(400).json({
-          status: "failed",
-          message: "El nombre debe contener entre 5 y 30 caracteres",
-          error: "El nombre debe contener entre 5 y 30 caracteres"
-        })
-      }
-
-      // Validar el nuevo email ingresado
-      if(user.email !== email) {
-        const checkNewEmail = await User.findOne({email: email});
-        if(checkNewEmail) {
+      try {
+        if(err) {
           return res.status(400).json({
             status: "failed",
-            message: "Ya existe un usuario asociado al email ingresado",
-            error: "Ya existe un usuario asociado al email ingresado"
+            message: "No se pudo procesar la información",
+            error: "No se pudo procesar la información"
           })
         }
-      }
-
-      if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
-        return res.status(400).json({
-          status: "failed",
-          message: "Debe introducir un email válido",
-          error: "Debe introducir un email válido"
-        })
-      }
-
-      // Validar la nueva contraseña
-      if(currentPassword) {
-        // Chequear si la contraseña es correcta antes de actualizarla
-        const checkPassword = await user.checkPassword(currentPassword, user.password);
-        if(!checkPassword) {
+  
+        // Extraer los campos del formulario
+        const {username, name, email, currentPassword, password, passwordConfirm, about} = fields;
+  
+        // Validar el username
+        if(username === "" || username.length < 5 || username.length > 30) {
           return res.status(400).json({
             status: "failed",
-            message: "Contraseña incorrecta",
-            error: "Contraseña incorrecta",
+            message: "El username debe contener entre 5 y 30 caracteres"
           })
         }
+  
+        // Validar el nombre
+        if(name === "" || name.length < 5 || name.length > 30) {
+          return res.status(400).json({
+            status: "failed",
+            message: "El nombre debe contener entre 5 y 30 caracteres",
+            error: "El nombre debe contener entre 5 y 30 caracteres"
+          })
+        }
+  
+        // Validar el email
+        if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+          return res.status(400).json({
+            status: "failed",
+            message: "Debe introducir un email válido",
+            error: "Debe introducir un email válido"
+          })
+        }
+  
+        // Validar la nueva contraseña
+        if(currentPassword) {
+          // Chequear si la contraseña es correcta antes de actualizarla
+          const checkPassword = await user.checkPassword(currentPassword, user.password);
+          if(!checkPassword) {
+            return res.status(400).json({
+              status: "failed",
+              message: "Contraseña incorrecta",
+              error: "Contraseña incorrecta",
+            })
+          }
+          
+          // Chequear si las contraseñas coinciden
+          if(password !== passwordConfirm) {
+            return res.status(400).json({
+              status: "failed",
+              message: "Las contraseñas no coinciden",
+              error: "Las contraseñas no coinciden",
+            })
+          }
+  
+          // Chequear que la contraseña contenga al menos 6 caracteres
+          if(password.length < 6) {
+            return res.status(400).json({
+              status: "failed",
+              message: "La contraseña debe contener al menos 6 caracteres",
+              error: "La contraseña debe contener al menos 6 caracteres"
+            })
+          }
+  
+          // Si todo es correcto, encriptar la nueva contraseña
+          const updatedPassword = await bcrypt.hash(password, 12);
+          await user.update({password: updatedPassword});
+        }
+  
+        // Validar que la foto del perfil no sea mayor de 1MB
+        if(files.photo) {
+          if(files.photo.size > 1000000) {
+            return res.status(400).json({
+              status: "failed",
+              message: "La foto no debe ser mayor de 1MB",
+              error: "La foto no debe ser mayor de 1MB"
+            });
+          }
+          user.photo.data = fs.readFileSync(files.photo.path);
+          user.photo.contentType = files.photo.type;
+        }
+  
+        // Actualizar información del usuario
+        user.username = username;
+        user.name = name;
+        user.email = email;
+        user.about = about;
+        user.profile = `${process.env.CLIENT_URL}/profile/${username}`;
+  
+        // Actualizar perfil en la base de datos
+        await user.save();
+    
+        // No enviar la foto del perfil al cliente
+        user.photo = null;
+
+        // No enviar la contraseña del usuario al cliente
+        user.password = null;
+    
+        // Enviar data actualizada del usuario
+        return res.json({
+          status: "success",
+          message: "Perfil actualizado exitosamente",
+          data: user
+        });
         
-        // Chequear si las contraseñas coinciden
-        if(password !== passwordConfirm) {
-          return res.status(400).json({
-            status: "failed",
-            message: "Las contraseñas no coinciden",
-            error: "Las contraseñas no coinciden",
-          })
+      } catch (error) {
+        // Manejo de errores de data duplicada
+        if(error.code && error.code === 11000) {
+          if(Object.keys(error.keyValue).includes("username")) {
+            return res.status(400).json({
+              status: "failed",
+              message: "El username ya fue utilizado por otro usuario",
+              error: error
+            })
+          }
+          if(Object.keys(error.keyValue).includes("email")) {
+            return res.status(400).json({
+              status: "failed",
+              message: "El email ya fue utilizado por otro usuario",
+              error: error
+            })
+          }
         }
-
-        // Chequear que la contraseña contenga al menos 6 caracteres
-        if(password.length < 6) {
-          return res.status(400).json({
-            status: "failed",
-            message: "La contraseña debe contener al menos 6 caracteres",
-            error: "La contraseña debe contener al menos 6 caracteres"
-          })
-        }
-
-        // Si todo es correcto, encriptar la nueva contraseña
-        const updatedPassword = await bcrypt.hash(password, 12);
-        await user.update({password: updatedPassword});
+        return res.status(500).json({
+          status: "failed",
+          message: "Internal server error",
+          error: error.message
+        })       
       }
-
-      // Validar que la foto del perfil no sea mayor de 1MB
-      if(files.photo) {
-        if(files.photo.size > 1000000) {
-          return res.status(400).json({
-            status: "failed",
-            message: "La foto no debe ser mayor de 1MB",
-            error: "La foto no debe ser mayor de 1MB"
-          });
-        }
-        user.photo.data = fs.readFileSync(files.photo.path);
-        user.photo.contentType = files.photo.type;
-      }
-
-      // Actualizar información del usuario
-      user.username = username;
-      user.name = name;
-      user.email = email;
-      user.about = about;
-      user.profile = `${process.env.CLIENT_URL}/profile/${username}`;
-
-      // Actualizar perfil en la base de datos
-      await user.save();
-
-      // No enviar la foto del perfil al cliente
-      user.photo = null;
-
-      return res.json({
-        status: "success",
-        message: "Perfil actualizado exitosamente",
-        data: user
-      })
-    });
+    }) 
 
   } catch (error) {
+    // Manejo de errores globales del proceso de actualización
     return res.status(500).json({
       status: "failed",
       message: "Internal server error",
@@ -185,7 +205,7 @@ exports.updateUserProfile = async (req, res) => {
 exports.getUserPhoto = async (req, res) => {
   try {
     // Buscar el usuario
-    const user = await User.find({username: req.params.username});
+    const user = await User.findOne({username: req.params.username});
 
     // Chaequear si el usuario existe
     if(!user) {
@@ -197,7 +217,7 @@ exports.getUserPhoto = async (req, res) => {
     }
 
     // Si existe, buscar su foto y enviarla al cliente
-    if(user.photo.data) {
+    if(user.photo) {
       res.set("Content-Type", user.photo.contentType);
       return res.send(user.photo.data);
     }
