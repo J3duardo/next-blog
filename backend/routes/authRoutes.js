583c8c5f411be06f294944bc195b0174753ba6d5
J3duardo <jesus_69eduardo@hotmail.com>
2020-03-04 10:17:41 -0400

    Backend: Limitar el número de solicitudes realizadas en ciertas rutas
    Se limita el número de intentos de login a 6 cada 10 minutos, se limita el número de cuentas creadas a 10 cada media hora y se limita el número de solicitudes de restablecimiento de contraseña a 5 por hora.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,15 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const expressLimiter = require("express-rate-limit");
 const {runValidation} = require("../middleware/validationMiddlewares");
 const {signupValidator, loginValidator, forgotPasswordValidator, resetPasswordValidator} = require("../middleware/validationMiddlewares/authValidators");
 const {preSignup, signup, createAccount, login, googleLogin, signout, forgotPassword, resetPassword, deleteUserAccount, protectRoute} = require("../controllers/authController");
 
+// Limitar el número de cuentas creadas desde la misma IP a 10 cada media hora
+const signupLimiter = expressLimiter({
+  windowMs: 30 * 60 * 1000,
+  max: 10,
+  message: {status: "failed", message: "Demasiadas cuentas creadas desde esta IP. Intente de nuevo más adelante"}
+});
+
+// Limitar el número de intentos de login a 6 cada 10 minutos
+const loginLimiter = expressLimiter({
+  windowMs: 10 * 60 * 1000,
+  max: 6,
+  message: {status: "failed", message: "Demasiados intentos de login. Por favor intente más adelante."}
+});
+
+// Limitar el número de solicitudes de restablecimiento de contraseñas a 5 por hora
+const forgotPasswordLimiter = expressLimiter({
+  windowMs: 60 * 60 * 1000,
+  max: 5,
+  message: {status: "failed", message: "Ha realizado demasiadas solicitudes. Intente de nuevo más adelante"}
+})
+
 // Registrar usuarios implementando activación de cuenta por email
-router.post("/pre-signup", signupValidator, runValidation, preSignup);
+router.post("/pre-signup", signupLimiter, signupValidator, runValidation, preSignup);
 // Registrar usuarios sin implementar activación de cuenta por email
-router.post("/signup", signupValidator, runValidation, signup);
+router.post("/signup", signupLimiter, signupValidator, runValidation, signup);
 // Iniciar sesión
-router.post("/login", loginValidator, runValidation, login);
+router.post("/login", loginLimiter, loginLimiter, loginValidator, runValidation, login);
 // Iniciar sesión con google
 router.post("/google-login", googleLogin);
 // Cerrar sesión
@@ -18,7 +40,7 @@ router.get("/signout", signout);
 // Activar la cuenta del usuario
 router.get("/activate-account", createAccount);
 // Enviar el token de reseteo de contraseña
-router.post("/forgot-password", forgotPasswordValidator, runValidation, forgotPassword);
+router.post("/forgot-password", forgotPasswordLimiter, forgotPasswordValidator, runValidation, forgotPassword);
 // Resetear la contraseña
 router.post("/reset-password", resetPasswordValidator, runValidation, resetPassword);
 // Eliminar cuenta de usuario
